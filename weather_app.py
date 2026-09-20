be5fb047a2b03b033975e17743bd077bb402e9a1
humai2606 <humairuljashira@gmail.com>
2026-09-20 22:23:11 +0530

    Refactor weather_app.py for clarity and structure

--- weather_app.py
+++ weather_app.py
@@ -3,7 +3,7 @@ import json
 import requests
 from  groq import Groq
 
-client = Groq(api_key=os.getenv("GROQ_API_KEY")
+client = Groq(api_key=os.getenv("GROQ_API_KEY"),
    base_url="https://api.groq.com/openai/v1"
 )
 
